6b3f1c4989aea09666df6a6e85573b5d2318a3a0
unknown <seutobaemeu@gmail.com>
2022-12-27 20:56:49 -0300

    Primeiro commit

---
+++ src/main/java/pi/quitter/quitter/QuitterApplication.java
@@ -0,0 +1,13 @@
+package pi.quitter.quitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pi/quitter/quitter/QuitterApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.quitter.quitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
