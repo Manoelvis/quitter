34bf6d39d7d4e1634a737e02d8f77b273f50e9c3
Manoelvis <manoelamarobarf.principal2022@gmail.com>
2022-12-28 16:09:00 -0300

    Configurando arquivos do BD

--- src/main/java/pi/quitter/quitter/models/Pergunta.java
+++ src/main/java/pi/quitter/quitter/models/Pergunta.java
@@ -1,9 +1,26 @@
 package pi.quitter.quitter.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pergunta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String pergunta;
 	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getPergunta() {
 		return pergunta;
