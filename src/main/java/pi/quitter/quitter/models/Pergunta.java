7a1bb7264c0993bf35dcbc440e87259f444b74ca
Manoelvis <manoelamarobarf.principal2022@gmail.com>
2022-12-28 13:51:45 -0300

    Criando o modelo de Pergunta

---
+++ src/main/java/pi/quitter/quitter/models/Pergunta.java
@@ -0,0 +1,23 @@
+package pi.quitter.quitter.models;
+
+public class Pergunta {
+
+	private String pergunta;
+	private String descricao;
+
+	public String getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
