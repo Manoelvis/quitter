41e39bca7d83aec045cee24d2c375c4d1f4b5fcc
Manoelvis <manoelamarobarf.principal2022@gmail.com>
2022-12-28 22:04:40 -0300

    Criando o repositório da classe Pergunta e configurando o método de criação de perguntas

--- src/main/java/pi/quitter/quitter/controllers/PerguntasController.java
+++ src/main/java/pi/quitter/quitter/controllers/PerguntasController.java
@@ -1,21 +1,29 @@
 package pi.quitter.quitter.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pi.quitter.quitter.models.Pergunta;
+import pi.quitter.quitter.repositories.PerguntaRepository;
 
 @Controller
 public class PerguntasController {
+
+	@Autowired
+	private PerguntaRepository pr;
 
 	@RequestMapping("/perguntas/form")
 	public String form() {
 		return "formPergunta";
 	}
-	
+
 	@PostMapping("/perguntas")
 	public String adicionarPergunta(Pergunta pergunta) {
-		return "evento-adicionado";
+
+		pr.save(pergunta);
+
+		return "pergunta-adicionada";
 	}
 }
