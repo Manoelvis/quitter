61d466ec71d3b51b4781ff982ebdf8793e8df674
Manoelvis <robson.comes@escolar.ifrn.edu.br>
2023-01-01 10:23:31 -0300

    Criando funcionalidade de listar as perguntas

--- src/main/java/pi/quitter/quitter/controllers/PerguntasController.java
+++ src/main/java/pi/quitter/quitter/controllers/PerguntasController.java
@@ -1,29 +1,43 @@
 package pi.quitter.quitter.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import pi.quitter.quitter.models.Pergunta;
 import pi.quitter.quitter.repositories.PerguntaRepository;
 
 @Controller
+@RequestMapping("/perguntas")
 public class PerguntasController {
 
 	@Autowired
 	private PerguntaRepository pr;
 
-	@RequestMapping("/perguntas/form")
+	@GetMapping("/form")
 	public String form() {
-		return "formPergunta";
+		return "perguntas/formPergunta";
 	}
 
-	@PostMapping("/perguntas")
+	@PostMapping
 	public String adicionarPergunta(Pergunta pergunta) {
 
 		pr.save(pergunta);
 
-		return "pergunta-adicionada";
+		return "perguntas/pergunta-adicionada";
+	}
+	
+	@GetMapping
+	public ModelAndView listarPerguntas() {
+		
+		List<Pergunta> perguntas = pr.findAll();
+		ModelAndView mv = new ModelAndView("perguntas/lista");
+		mv.addObject("perguntas", perguntas);
+		return mv;
 	}
 }
