4b676a11fbe225c7de9bb496fa9e066cc942d179
Manoelvis <amaro.mabf.principal2023@gmail.com>
2023-01-05 10:05:03 -0300

    Criando repositório para usuário

---
+++ src/main/java/pi/quitter/quitter/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package pi.quitter.quitter.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import pi.quitter.quitter.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+
+	Usuario findByMatricula(String matricula);
+}
